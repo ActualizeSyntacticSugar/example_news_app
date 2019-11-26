f603c43100cf2baf065b0c494122b2e207eff0a5
Peter Jang <peter.x.jang@gmail.com>
2019-11-25 21:01:50 -0600

    Add example action using NewsAPI (with figaro to manage environment variables)

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -1,2 +1,10 @@
 class Api::ArticlesController < ApplicationController
+  def index
+    # @articles = Article.all
+    # render "index.json.jb"
+
+    response = HTTP.get("https://newsapi.org/v2/everything?q=#{params[:input_search]}&apiKey=#{ENV["NEWS_API_KEY"]}")
+    articles = response.parse["articles"]
+    render json: { result: articles }
+  end
 end
